153a75b58164ae71e24386913ba9e7396acd4dea
lizongze <leo.ze@qq.com>
2025-05-18 11:38:42 +0800

    refactor: change class into function

--- src/main/app/AppRouter.tsx
+++ src/main/app/AppRouter.tsx
@@ -1,16 +1,10 @@
-import * as React from 'react';
 import { createHashRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
 // import history from './router-history';
 import { routers as normalRoutes } from '@main/router-define';
 
-export class AppRouter extends React.PureComponent<{ hide?: boolean }> {
-  render() {
+export const AppRouter = (props: { hide?: boolean, store?: {} }) => {
     // const routerElement = createBrowserRouter(normalRoutes);
     const routerElement = createHashRouter(normalRoutes);
-
-    // N.B: hide for prefetch AppStartUp chunk after LoginInterceptor mounted
     return <RouterProvider router={routerElement} />;
-  }
 }
 
-export default AppRouter;
