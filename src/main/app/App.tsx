487901285ea998c44bd4b0572d264959f103bbe8
lizongze <leo.ze@qq.com>
2025-05-18 11:39:05 +0800

    chore: prettier

--- src/main/app/App.tsx
+++ src/main/app/App.tsx
@@ -4,18 +4,17 @@ import { AppRouter } from './AppRouter';
 
 const globalMobxStore = {};
 
-export const App = (props: { store; hide?: boolean }) =>  {
-
-    const { store: curStore, hide } = props;
-    const store = curStore || globalMobxStore;
-    // N.B: hide for prefetch AppStartUp chunk after LoginInterceptor mounted
-    return (
-      <MobxProvider store={store}>
-        <Suspense>
-          <AppRouter store={store} />
-        </Suspense>
-      </MobxProvider>
-    );
-}
+export const App = (props: { store; hide?: boolean }) => {
+  const { store: curStore, hide } = props;
+  const store = curStore || globalMobxStore;
+  // N.B: hide for prefetch AppStartUp chunk after LoginInterceptor mounted
+  return (
+    <MobxProvider store={store}>
+      <Suspense>
+        <AppRouter store={store} />
+      </Suspense>
+    </MobxProvider>
+  );
+};
 
 export default App;
--- src/main/app/AppRouter.tsx
+++ src/main/app/AppRouter.tsx
@@ -2,9 +2,8 @@ import { createHashRouter, RouterProvider, createBrowserRouter } from 'react-rou
 // import history from './router-history';
 import { routers as normalRoutes } from '@main/router-define';
 
-export const AppRouter = (props: { hide?: boolean, store?: {} }) => {
-    // const routerElement = createBrowserRouter(normalRoutes);
-    const routerElement = createHashRouter(normalRoutes);
-    return <RouterProvider router={routerElement} />;
-}
-
+export const AppRouter = (props: { hide?: boolean; store?: {} }) => {
+  // const routerElement = createBrowserRouter(normalRoutes);
+  const routerElement = createHashRouter(normalRoutes);
+  return <RouterProvider router={routerElement} />;
+};
